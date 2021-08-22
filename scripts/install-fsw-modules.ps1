ee5452ee93955b726ddfbc442c02293f5a04a421
carrollh <heath.aaron.carroll@gmail.com>
2021-08-22 10:40:39 -0400

    working on dsc cert creation for 2012r2

---
+++ scripts/install-fsw-modules.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)]
+    [Switch]$WS2012R2
+)
+
+"Setting Execution Policy to Remote Signed"
+Set-ExecutionPolicy RemoteSigned -Force
+
+"Setting up Powershell Gallery to Install DSC Modules"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+"Installing the needed Powershell DSC modules for this Quick Start"
+Install-Module -Name PSDscResources
+Install-Module -Name xSmbShare
+
+"Disabling Windows Firewall"
+Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled False
+
+"Creating Directory for DSC Public Cert"
+New-Item -Path C:\AWSQuickstart\publickeys -ItemType directory -Force
+
+"Removing DSC Certificate if it already exists"
+(Get-ChildItem Cert:\LocalMachine\My\) | Where-Object { $_.Subject -eq "CN=AWSQSDscEncryptCert" } | Remove-Item
+
+If($WS2012R2) {
+    $cert = New-SelfSignedCertificate -DnsName 'AWSQSDscEncryptCert' -CertStoreLocation 'Cert:\LocalMachine\My'
+}
+else {
+    $cert = New-SelfSignedCertificate -Type DocumentEncryptionCertLegacyCsp -DnsName 'AWSQSDscEncryptCert' -HashAlgorithm SHA256
+    # Exporting the public key certificate
+    $cert | Export-Certificate -FilePath "C:\AWSQuickstart\publickeys\AWSQSDscPublicKey.cer" -Force
+}
